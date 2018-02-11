f2e73a886e153a43e3bbb6df85bc25c0f2eabe52
yangchao <chao_china@163.com>
2018-02-11 13:58:09 +0800

    修复ssh登录带参数时解析错误无法登录bug

--- 02_linux/ssh_quick_login/sshautologin.sh
+++ 02_linux/ssh_quick_login/sshautologin.sh
@@ -5,8 +5,9 @@
 
 set LOGIN_CMD [lindex $argv 0]
 set LOGIN_PSW [lindex $argv 1]
+set LOGIN_PARAM [lindex $argv 2]
 set timeout 3
-spawn ssh "$LOGIN_CMD"
+spawn ssh "$LOGIN_CMD" "$LOGIN_PARAM"
 expect {
     "yes/no" {exp_send "yes\r";exp_continue}
     "password:" {exp_send "${LOGIN_PSW}\r"}
