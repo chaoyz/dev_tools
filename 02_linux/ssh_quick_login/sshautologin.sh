d52a6614ff40b3a25fe57b3b2c370b65f6ff358f
yangchao <chao_china@163.com>
2018-03-27 18:10:11 +0800

    修改ssh等待输入密码的时间由3s增加到30s

--- 02_linux/ssh_quick_login/sshautologin.sh
+++ 02_linux/ssh_quick_login/sshautologin.sh
@@ -6,7 +6,7 @@
 set LOGIN_CMD [lindex $argv 0]
 set LOGIN_PSW [lindex $argv 1]
 set LOGIN_PARAM [lindex $argv 2]
-set timeout 3
+set timeout 30
 spawn ssh "$LOGIN_CMD" "$LOGIN_PARAM"
 expect {
     "yes/no" {exp_send "yes\r";exp_continue}
